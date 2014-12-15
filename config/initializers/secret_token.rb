89f35fa5142dba3d3b623990192830762b28354b
vianull <vianull@outlook.com>
2014-12-16 00:44:05 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorWeibo::Application.config.secret_token = '5557eaaf2b52d057393e6e659f703470d107b0d92809d428393e057113be290d2ee342a32d10962cb9b91507713332604257acf76d666a2e7ca989981c3c60a5'
